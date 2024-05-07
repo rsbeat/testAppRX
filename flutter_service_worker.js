c3049e8f1b47afb450dfc2f53465a50875117328
rsbeat <hamed1380r@gmail.com>
2024-05-07 14:32:01 +0330

    release Mode

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -61,9 +61,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "d5025c0e8a27daca692c7470283b88e8
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "da5a2f1c9346738074de4c001194f05d",
-"/": "da5a2f1c9346738074de4c001194f05d",
-"main.dart.js": "ce80c5bf6f2a5d05b1de3273e9c0eccd",
+"index.html": "224fabcedb2255e567699a75e3fec60c",
+"/": "224fabcedb2255e567699a75e3fec60c",
+"main.dart.js": "bb82060389ae6956f3db01b5cc08f700",
 "manifest.json": "4b69bdd468dd84cd49218170c513e435",
 "version.json": "8f5f454ec0e368fb08d0f6ccce01830d"};
 // The application shell files that are downloaded before a service worker can
